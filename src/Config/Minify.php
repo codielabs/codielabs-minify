f127cfc963b05fcedca7c083b622f4e858da5aab
Ryan Swan <imenxboyssan@gmail.com>
2020-06-11 17:47:14 +0700

    Update code

---
+++ src/Config/Minify.php
@@ -0,0 +1,12 @@
+<?php namespace Codielabs\Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Minify extends BaseConfig
+{
+
+    public minifyEnabled = true;
+
+    public minifyAssetsDir = 'assets';
+
+}
--- src/Minify.php
+++ src/Minify.php
@@ -1,11 +1,22 @@
 <?php namespace Codielabs;
 
+use Codielabs\Config\Minify;
+
 class Minify
 {
 
+    protected $config;
+    
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->config = new Minify();
+    }    
+
     public function start()
     {
-        return 'Minify Loaded';
+        return $this->config;
     }
 
 }
